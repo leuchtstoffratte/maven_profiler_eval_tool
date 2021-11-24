a23c20a01203c1ae3795d63a1d8ba104da95ddbc
leuchtstoffratte <kitersquirel@web.de>
2021-11-25 00:04:37 +0100

    Parsing of json works
    Introduced (some kind of) module structure

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,98 @@
-use serde::{Deserialize, Serialize};
-use serde_json::{Result, Value};
+use std::fs;
 
-
-#[derive(Serialize, Deserialize)]
-struct MojoBuildTime{
-    mojo: String,
-    time: String
-}
-
-
-#[derive(Serialize, Deserialize)]
-struct ProjectBuildTimeList{
-    project : String,
-    time : String,
-    mojos : Vec<MojoBuildTime>
-}
 
 fn main(){
-    let reading_went_well = test_read_mojo_list();
+    // let reading_went_well = test_read_mojo_list();
 
-    if reading_went_well.is_ok(){
-        println!("yeah");
-    }else{
-        println!("möö");
+    let file_name = "E:\\Rattenmann\\Coding\\Rust\\maven_profiler_summation\\samples\\maven_profiler_sample_jsons.txt";
+
+    let report_content = fs::read_to_string(file_name).expect("reading file failed.");
+    
+    let reading_went_well = crate::parsing_components::parse_maven_profiler_report(&report_content).expect("extracting json data failed");
+
+    let project_name = reading_went_well.name;
+    let n_project = reading_went_well.projects.len();
+    let build_duration = sum_single_project_build_time(&reading_went_well.projects[0]);
+
+
+    println!("Project {} contained {} subprojects and they took {} ms to build.", project_name,n_project, build_duration);
+
+
+    
+}
+
+
+
+
+
+fn sum_single_project_build_time (projekt_json : &parsing_components::ProjectBuildTimeList) -> i64{
+    let mut sum_of_build_times : i64  = 0;
+
+    for m in projekt_json.mojos.iter() {
+        sum_of_build_times += parsing_components::parse_time_in_ms(&m.time);
+
     }
+
+    sum_of_build_times
 }
 
 
-
-
-fn test_read_mojo() -> Result<()> {
-
-    let test_mojo = r#"       {
-        "mojo": "org.apache.maven.plugins:maven-compiler-plugin:3.6.0:testCompile {execution: default-testCompile}",
-        "time": "2 ms"
-      }"#;
-
-
-    let m :Value = serde_json::from_str(test_mojo)?;
-
-
-    println!("{} took {} to build", m["mojo"], m["time"]);
-
-    Ok(())
-}
-
-
-
-fn test_read_mojo_list ()-> Result<()>{
-
-    let mojo_list_str = r#"    {
-        "project": "maven-profiler",
-        "time": "43378 ms",
-        "mojos": [
-          {
-            "mojo": "org.apache.maven.plugins:maven-invoker-plugin:2.0.0:run {execution: integration-test}",
-            "time": "30706 ms"
-          },
-          {
-            "mojo": "org.apache.maven.plugins:maven-surefire-plugin:2.19.1:test {execution: default-test}",
-            "time": "7300 ms"
-          },
-          {
-            "mojo": "org.apache.maven.plugins:maven-shade-plugin:2.4.3:shade {execution: default}",
-            "time": "1378 ms"
-          },
-          {
-            "mojo": "org.apache.maven.plugins:maven-compiler-plugin:3.6.0:compile {execution: default-compile}",
-            "time": "1112 ms"
-          },
-          {
-            "mojo": "org.codehaus.gmavenplus:gmavenplus-plugin:1.5:testCompile {execution: default}",
-            "time": "1102 ms"
-          },
-          {
-            "mojo": "org.apache.maven.plugins:maven-invoker-plugin:2.0.0:install {execution: integration-test}",
-            "time": "293 ms"
-          },
-          {
-            "mojo": "org.apache.maven.plugins:maven-enforcer-plugin:1.4.1:enforce {execution: enforce-maven}",
-            "time": "225 ms"
-          }
-        ]
-      }"#;
-
-      let mojo_list : ProjectBuildTimeList = serde_json::from_str(mojo_list_str)?;
-
-      let mut sum_build_times : i64 = 0;
-
-
-      for mojo in mojo_list.mojos.iter(){
-        sum_build_times += parse_time_in_ms(&mojo.time);
-        println!("Mojo {} took {} to build", mojo.mojo, mojo.time);
-      }
-
-
-      println!(" project {} took {} to build. It contains {} separate mojos", mojo_list.project, mojo_list.time, mojo_list.mojos.len());
-
-      println!(" the entire build time was {} ms ", sum_build_times);
-
-      Ok(())
-}
-
-
-
-
-/**
- * return -10 if parsing failed
- */
-fn parse_time_in_ms (time_str : &str) -> i64{
+mod parsing_components {
+    use serde::{Deserialize, Serialize};
+    use serde_json::{Result};
   
-  let time_number = time_str.strip_suffix(" ms");
-  if time_number.is_some(){
-    time_number.unwrap().parse().expect("")
-  } else { -1 }
-}
+    #[derive(Serialize, Deserialize)]
+    pub struct MojoBuildTime{
+        pub mojo: String,
+        pub time: String
+    }
 
 
-fn sum_single_project_build_time (projekt_json : ProjectBuildTimeList) -> i64{
-  0
+    #[derive(Serialize, Deserialize)]
+    pub struct ProjectBuildTimeList{
+        project : String,
+        time : String,
+        pub mojos : Vec<MojoBuildTime>
+    }
+
+
+    #[derive(Serialize, Deserialize)]
+    pub struct MavenProfilerReport{
+      pub name : String,
+      profile_name : String,
+      time : String,
+      goals: String,
+      date : String,
+      parameters: String,
+      pub projects: Vec<ProjectBuildTimeList>
+    }
+
+
+
+    pub fn parse_maven_profiler_report( report:  &str ) -> Result<MavenProfilerReport>{
+
+        let result : MavenProfilerReport = serde_json::from_str(report)?;
+      
+        println!(" paresed {}", result.name);
+      
+        Ok(result)
+    }
+    
+    
+    
+
+  /**
+   * return -10 if parsing failed
+   */
+  pub fn parse_time_in_ms (time_str : &str) -> i64{
+    
+      let time_number = time_str.strip_suffix(" ms");
+      if time_number.is_some(){
+        time_number.unwrap().parse().expect("")
+      } else { -1 }
+    }
+  
+  
 }
+  
+  
